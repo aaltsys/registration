c33f114271a6951962b8d4bbad4d257f395d1914
Mike Cochran <mcochran@linux.com>
2012-01-20 14:26:26 -0600

    Added webmin

--- registration.sh
+++ registration.sh
@@ -4,6 +4,10 @@ aptitude -y install ruby-full unzip openvpn screen dialog python-software-proper
 add-apt-repository ppa:hplip-isv/ppa
 aptitude update
 aptitude install hpijs hpijs-ppds hplip hplip-cups hplip-data
+wget http://www.webmin.com/download/deb/webmin-current.deb -o /tmp/webmin.deb
+dpkg -i /tmp/webmin.deb
+apt-get -f install
+rm /tmp/webmin.deb
 
 wget https://raw.github.com/aaltsys/registration/master/registration.rb -o /tmp/reg.rb
 
