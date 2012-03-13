9a2f9bf3f6327acc7e5715edd40e57c5aafa1fa3
Mike Cochran <mcochran@linux.com>
2012-03-13 12:28:35 -0500

    Fix webmin download

--- registration.sh
+++ registration.sh
@@ -4,7 +4,7 @@ aptitude -y install ruby-full unzip openvpn screen dialog python-software-proper
 add-apt-repository ppa:hplip-isv/ppa
 aptitude update
 aptitude install hpijs hpijs-ppds hplip hplip-cups hplip-data
-wget http://www.webmin.com/download/deb/webmin-current.deb -o /tmp/webmin.deb
+wget http://www.webmin.com/download/deb/webmin-current.deb -O /tmp/webmin.deb
 dpkg -i /tmp/webmin.deb
 apt-get -f install
 rm /tmp/webmin.deb
