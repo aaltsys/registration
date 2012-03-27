a5e1a2d532c988ba462ef64be5aff358c025f9a4
Mike Cochran <mcochran@linux.com>
2012-03-27 12:24:01 -0500

    Loop on bad registration

--- registration.rb
+++ registration.rb
@@ -20,57 +20,59 @@ def verify_reg(reg)
     return false
   end
 end
-
-while true
-  puts 
-  print "Registration Number: "
-  STDOUT.flush
-  registration_number = STDIN.gets.chomp!
-  break if verify_reg(registration_number)
-  puts "Invalid Registration Number!"
-end
-
-while true
-  puts
-  print "Activation Code: "
-  activation_code = STDIN.gets.chomp!
-  break if (!activation_code.nil? and !activation_code.empty?)
-  puts "Invalid Activation Code!"
-end
-
-while true
-  puts 
-  print "Email Address: "
-  email = STDIN.gets.chomp!
-  break if email.include?("@")
-  puts "Invalid Email Address!"
-end
-
-begin
-  res = Net::HTTP.get_response(URI.parse("http://register.aaltsys.net/activate?registration_number=#{registration_number}&activation=#{activation_code}&email=#{email}&version=#{version}"))
-
-  case res
-  when Net::HTTPOK
-    open("/tmp/install.zip", "wb") { |file|
-      file.write(res.body)
-    }
-    system("mkdir -p /tmp/install/")
-    system("unzip /tmp/install.zip -d /tmp/install/")
-    system("cd /tmp/install/; /bin/bash /tmp/install/install.sh")
-    system("rm -rf /tmp/install*")
-    success = true
-  else
-    success = false
-    error = res.message
+success = false
+unless success
+  while true
+    puts 
+    print "Registration Number: "
+    STDOUT.flush
+    registration_number = STDIN.gets.chomp!
+    break if verify_reg(registration_number)
+    puts "Invalid Registration Number!"
   end
-rescue
-  success = false
-  error ||= "Unknown error"
-end
 
-if success
-  puts "Registration successful"
-else
-  puts "Error during registration: "
-  puts error
+  while true
+    puts
+    print "Activation Code: "
+    activation_code = STDIN.gets.chomp!
+    break if (!activation_code.nil? and !activation_code.empty?)
+    puts "Invalid Activation Code!"
+  end
+
+  while true
+    puts 
+    print "Email Address: "
+    email = STDIN.gets.chomp!
+    break if email.include?("@")
+    puts "Invalid Email Address!"
+  end
+
+  begin
+    res = Net::HTTP.get_response(URI.parse("http://register.aaltsys.net/activate?registration_number=#{registration_number}&activation=#{activation_code}&email=#{email}&version=#{version}"))
+
+    case res
+    when Net::HTTPOK
+      open("/tmp/install.zip", "wb") { |file|
+        file.write(res.body)
+      }
+      system("mkdir -p /tmp/install/")
+      system("unzip /tmp/install.zip -d /tmp/install/")
+      system("cd /tmp/install/; /bin/bash /tmp/install/install.sh")
+      system("rm -rf /tmp/install*")
+      success = true
+    else
+      success = false
+      error = res.message
+    end
+  rescue
+    success = false
+    error ||= "Unknown error"
+  end
+
+  if success
+    puts "Registration successful"
+  else
+    puts "Error during registration: "
+    puts error
+  end
 end
