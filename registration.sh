3d55c6031f6c8a0acc366186664dbbc7705f5eae
Gerald Lovel <gerald@lovels.us>
2014-05-08 15:21:02 -0500

    updates to the registration script

--- registration.sh
+++ registration.sh
@@ -1,49 +1,73 @@
 #! /usr/bin/env bash
 
-# webmin
-if [ ! -d '/etc/webmin' ] ; then
-   wget http://www.webmin.com/download/deb/webmin-current.deb -O /tmp/webmin.deb
-   dpkg -i /tmp/webmin.deb
-   apt-get -y -f install
-   rm /tmp/webmin.deb
-fi
+echo "Register and configure AAltsys openVPN client"
 
-# ruby-full
-if [ ! -d '/usr/bin/ruby' ] ; then
-   apt-get -y install ruby-full
-fi
+if [[ $EUID -ne 0 ]] ; then echo -e "\e[1;31m try again using sudo \e[0m" ; exit 1 ; fi
 
-# openvpn
-apt-get -y install openvpn
+# Install ruby-full and openvpn if missing
+APT=0
+PKGS="ruby-full openvpn"
+for i in $PKGS
+do
+   dpkg -s $i > null
+   if [ $? -ne 0 ] ; then
+      APT=1
+      echo "$i is missing, it will be installed"
+      apt-get -y install $i
+   fi
+done
 
-#add-apt-repository ppa:hplip-isv/ppa
-# hplip and other utilities
-apt-get -y install hpijs hpijs-ppds hplip hplip-cups hplip-data
-apt-get -y install autofs byobu screen links lynx unzip dialog gdebi
-apt-get -y install python-software-properties ntfsprogs curl smbclient
-
-apt-get -y -f install
-apt-get -y update
-
-wget https://raw.github.com/aaltsys/registration/master/registration.rb -O /tmp/reg.rb
+# wget https://raw.github.com/aaltsys/registration/master/registration.rb -O /tmp/reg.rb
+wget https://raw.github.com/aaltsys/doc-servers/master/resources/_downloads/registration.rb -O /tmp/reg.rb
 /usr/bin/ruby /tmp/reg.rb
 rm /tmp/reg.rb
 echo "Press enter to continue..."
 read
 
 # create default script variables for aaltsysvpn
-sudo bash < <(echo 'echo "AUTOSTART=\"aaltsys\"" > /etc/default/aaltsysvpn')
+# bash < <(echo 'echo "AUTOSTART=\"aaltsys\"" > /etc/default/aaltsysvpn')
+echo "AUTOSTART=\"aaltsys\"" > /etc/default/aaltsysvpn
 # create boot init script for aaltsysvpn (copied from openvpn script)
-sudo cp /etc/init.d/openvpn /etc/init.d/aaltsysvpn
+cp /etc/init.d/openvpn /etc/init.d/aaltsysvpn
 # edit script to point to aaltsysvpn default script variables
-sudo sed -i s/"default\/openvpn"/"default\/aaltsysvpn"/ /etc/init.d/aaltsysvpn
+sed -i s/"default\/openvpn"/"default\/aaltsysvpn"/ /etc/init.d/aaltsysvpn
 # create run-level pointers to the aaltsysvpn script
-sudo update-rc.d aaltsysvpn defaults
+update-rc.d aaltsysvpn defaults
 # change vpn configuration "client" to "aaltsys"
-sudo mv /etc/openvpn/client.conf /etc/openvpn/aaltsys.conf
+mv /etc/openvpn/client.conf /etc/openvpn/aaltsys.conf
 
-sudo invoke-rc.d aaltsysvpn start
+invoke-rc.d aaltsysvpn start
 
 mkdir -p /home/mnt/backup/source_config
 
-apt-get -y upgrade
+# webmin
+if [ ! -d '/etc/webmin' ] ; then
+   echo "Installing Webmin"
+   wget http://www.webmin.com/download/deb/webmin-current.deb -O /tmp/webmin.deb
+   dpkg -i /tmp/webmin.deb
+   apt-get -y -f install
+   rm /tmp/webmin.deb
+fi
+
+# add-apt-repository ppa:hplip-isv/ppa
+# hplip and other utilities
+echo "Installing additional packages"
+PKGS="hpijs hpijs-ppds hplip hplip-cups hplip-data autofs screen links lynx unzip dialog python-software-properties ntfs-3g ntfsprogs curl smbclient"
+for i in $PKGS
+do
+   dpkg -s $i > null
+   if [ $? -ne 0 ] ; then
+      APT=1
+      echo "$i is missing, it will be installed"
+      apt-get -y install $i
+   fi
+done
+
+if [ $APT -ne 0 ] 
+then
+   echo -e  "\e[1;31m Updating system packages, this may take a while \e[0m"
+   apt-get -y -f install
+   apt-get -y update && apt-get -y dist-upgrade
+fi
+
+exit 0
